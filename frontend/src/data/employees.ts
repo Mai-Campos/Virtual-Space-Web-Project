a0a0ab2944ccf07ecf574618c3b6b5f5a5c0cd66
Maikol  Campos <Camposmaikol1@gmail.com>
2025-12-14 21:15:56 -0500

    test: Mocks de datos empleados

---
+++ frontend/src/data/employees.ts
@@ -0,0 +1,16 @@
+export const employees = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "jhon@gmail.com",
+    password: "password123",
+    roles: ["Employee", "User"],
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    email: "jane@gmail.com",
+    password: "jane1234",
+    roles: ["Employee"],
+  },
+];
